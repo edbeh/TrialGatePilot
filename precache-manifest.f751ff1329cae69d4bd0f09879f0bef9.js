048cb96ec9e8f4c27fd7ddf95ce485639bf0625e
edbeh <edbeh89@gmail.com>
2020-05-11 14:34:14 +0800

    Updates

---
+++ precache-manifest.f751ff1329cae69d4bd0f09879f0bef9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3be8ee46794827de7a01a2561d5220",
+    "url": "/TrialGatePilot/index.html"
+  },
+  {
+    "revision": "82f116bb0d0754955bfa",
+    "url": "/TrialGatePilot/static/css/main.c02ff2fc.chunk.css"
+  },
+  {
+    "revision": "fa6c107ae45324230764",
+    "url": "/TrialGatePilot/static/js/2.5f393b9c.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/TrialGatePilot/static/js/2.5f393b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f116bb0d0754955bfa",
+    "url": "/TrialGatePilot/static/js/main.632e870e.chunk.js"
+  },
+  {
+    "revision": "ddbb635be904b40a93ef",
+    "url": "/TrialGatePilot/static/js/runtime-main.d0186b07.js"
+  },
+  {
+    "revision": "2515bf7df06d75a57d5f930464aca7b3",
+    "url": "/TrialGatePilot/static/media/logo.2515bf7d.svg"
+  },
+  {
+    "revision": "3f46d8aa3534ab203bcef9d4ac8ca5fe",
+    "url": "/TrialGatePilot/static/media/not-found.3f46d8aa.jpg"
+  }
+]);
