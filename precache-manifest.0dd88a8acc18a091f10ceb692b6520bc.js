0fc5db2a4c057ee7299af77eeaac229a3cc91fc5
edbeh <edbeh89@gmail.com>
2020-05-11 17:03:18 +0800

    Updates

--- precache-manifest.0dd88a8acc18a091f10ceb692b6520bc.js
+++ precache-manifest.0dd88a8acc18a091f10ceb692b6520bc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "10cfe768ed70439ab63fdf209a7afeed",
+    "revision": "fbc6d99f12bfb05225bd4a2c617f1c61",
     "url": "/TrialGatePilot/index.html"
   },
   {
-    "revision": "435874634004c87a6d2e",
-    "url": "/TrialGatePilot/static/css/main.513de38f.chunk.css"
+    "revision": "79a7a510b68beab4f276",
+    "url": "/TrialGatePilot/static/css/main.f665e2d0.chunk.css"
   },
   {
-    "revision": "8f1c72aad36317d71476",
-    "url": "/TrialGatePilot/static/js/2.475fcbe3.chunk.js"
+    "revision": "9458626be001a450409b",
+    "url": "/TrialGatePilot/static/js/2.6ea6b4bf.chunk.js"
   },
   {
     "revision": "eac4cf64bf573541fd6dba29c78393a0",
-    "url": "/TrialGatePilot/static/js/2.475fcbe3.chunk.js.LICENSE.txt"
+    "url": "/TrialGatePilot/static/js/2.6ea6b4bf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "435874634004c87a6d2e",
-    "url": "/TrialGatePilot/static/js/main.2161767e.chunk.js"
+    "revision": "79a7a510b68beab4f276",
+    "url": "/TrialGatePilot/static/js/main.063a2d56.chunk.js"
   },
   {
     "revision": "ddbb635be904b40a93ef",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TrialGatePilot/precache-manifest.97805c2ce00571ac1658febc7a80e1fb.js"
+  "/TrialGatePilot/precache-manifest.0dd88a8acc18a091f10ceb692b6520bc.js"
 );
 
 self.addEventListener('message', (event) => {
